2f2706ea1c42c83c3701cf536ddf4d4ccffeb803
Sergey Osokin <osokin.5642@gmail.com>
2022-10-06 16:46:22 +0500

    Initial Commit

---
+++ SwiftUIText/SwiftUITextApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITextApp.swift
+//  SwiftUIText
+//
+//  Created by admin on 06.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
